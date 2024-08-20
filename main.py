0f38f4c1dac9e5add703b4c36ad735ba54617f77
James Sargant <shorinbonsai@gmail.com>
2024-08-19 21:15:05 -0400

    stuff

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ def main():
     print("Starting asteroids!")
     print("Screen width: " + str(SCREEN_WIDTH))
     print("Screen height: " + str(SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        tmp = clock.tick(60)
+        dt = tmp/1000
 
 
 if __name__ == "__main__":
